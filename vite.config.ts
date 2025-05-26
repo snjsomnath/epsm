3afbfcb23dc005c99df25a3e98c3f41537ea3787
Sanjay Somanath <53001297+snjsomnath@users.noreply.github.com>
2025-05-26 08:10:20 +0200

    Fix Vite dependency resolution

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    include: ['lucide-react'],
+    exclude: ['lucide-react'],
   },
-  server: {
-    force: true // Force dependency pre-bundling
-  }
 });
