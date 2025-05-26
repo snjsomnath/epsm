2af000d2e57e19fe3ff992a3ccbdf3ccf4ca0f24
Sanjay Somanath <53001297+snjsomnath@users.noreply.github.com>
2025-05-26 10:09:14 +0200

    Add scenario tables to database

--- src/lib/database.types.ts
+++ src/lib/database.types.ts
@@ -7,9 +7,13 @@ export type WindowGlazing = Database['public']['Tables']['window_glazing']['Row'
 export type Construction = Database['public']['Tables']['constructions']['Row'];
 export type Layer = Database['public']['Tables']['layers']['Row'];
 export type ConstructionSet = Database['public']['Tables']['construction_sets']['Row'];
+export type Scenario = Database['public']['Tables']['scenarios']['Row'];
+export type ScenarioConstruction = Database['public']['Tables']['scenario_constructions']['Row'];
 
 export type MaterialInsert = Database['public']['Tables']['materials']['Insert'];
 export type WindowGlazingInsert = Database['public']['Tables']['window_glazing']['Insert'];
 export type ConstructionInsert = Database['public']['Tables']['constructions']['Insert'];
 export type LayerInsert = Database['public']['Tables']['layers']['Insert'];
 export type ConstructionSetInsert = Database['public']['Tables']['construction_sets']['Insert'];
+export type ScenarioInsert = Database['public']['Tables']['scenarios']['Insert'];
+export type ScenarioConstructionInsert = Database['public']['Tables']['scenario_constructions']['Insert'];
