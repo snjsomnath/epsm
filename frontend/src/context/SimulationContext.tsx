0faeb4875a211521ecd71a2c9caed18b223b6ed9
snjsomnath <snjsomnath@gmail.com>
2025-09-22 23:47:16 +0200

    refactor(SimulationContext): remove unused import of React

--- frontend/src/context/SimulationContext.tsx
+++ frontend/src/context/SimulationContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState, ReactNode } from 'react';
+import { createContext, useContext, useState, ReactNode } from 'react';
 import { uniqueNamesGenerator, adjectives, animals } from 'unique-names-generator';
 const makeFriendlyName = (opts?: { separator?: string }) => {
   try {
